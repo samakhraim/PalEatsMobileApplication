cf42e8b992e370d3b8d0396cd6e4222f9429b6f6
osamashouli <101651050+osamashouli@users.noreply.github.com>
2023-04-13 22:30:37 +0300

    Initial Commit

---
+++ PalEats/PalEats/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using PalEats.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PalEats
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new SignUpPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PalEats/PalEats/ViewModels/SignInPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalEats.ViewModels
+{
+    internal class SignInPageViewModel
+    {
+    }
+}
