422c4ab3f706aacbc53c6109686f9ed27014ce0d
maya-abdoh <92648274+maya-abdoh@users.noreply.github.com>
2023-04-25 00:25:00 +0200

    PE 163 finished build favorite card component

--- PalEats/PalEats/ViewModels/FavoritePageViewModel.cs
+++ PalEats/PalEats/ViewModels/FavoritePageViewModel.cs
@@ -1,10 +1,55 @@
-﻿using System;
+﻿using PalEats.Models;
+using PalEats.Services;
+using PalEats.Views;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace PalEats.ViewModels
 {
-    internal class FavoritePageViewModel
+    public class FavoritePageViewModel : INotifyPropertyChanged
     {
+        private readonly FavoritePageServices favoritePageServices;
+
+        public FavoritePageViewModel()
+        {
+            favoritePageServices = new FavoritePageServices();
+            LoadFavoriteAsync();
+        }
+        private List<Recipe> categories = new List<Recipe>();
+
+        public List<Dish> dishes
+        {
+            get { return dishes; }
+            set
+            {
+                dishes = value;
+                OnPropertyChanged(nameof(dishes));
+            }
+        }
+
+        public async Task LoadFavoriteAsync()
+        {
+            try
+            {
+                dishes = await favoritePageServices.GetFavoriteDishesAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error while loading categories: {ex.Message}");
+
+                await App.Current.MainPage.DisplayAlert("Error", "An error occurred while loading categories. Please try again later.", "OK");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
