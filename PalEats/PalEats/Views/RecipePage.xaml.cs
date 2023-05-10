94b8583229a7ca6845b1c61087ad72309779f179
osamashouli <101651050+osamashouli@users.noreply.github.com>
2023-05-10 17:17:29 +0300

    PE-297-build-the-shopping-image-button

--- PalEats/PalEats/Views/RecipePage.xaml.cs
+++ PalEats/PalEats/Views/RecipePage.xaml.cs
@@ -1,14 +1,13 @@
-﻿
+﻿using PalEats.Views;
 using PalEats.Models;
 using PalEats.ViewModels;
-using Rg.Plugins.Popup.Services;
 using System;
 using System.Linq;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-    namespace PalEats.Views
+namespace PalEats.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RecipePage : ContentPage
@@ -21,9 +20,9 @@ using Xamarin.Forms.Xaml;
             InitializeComponent();
             viewModel = new RecipePageViewModel(id);
             BindingContext = viewModel;
-
+           
         }
-    
+
 
         private void ShareButton_Clicked(object sender, EventArgs e)
         {
@@ -36,7 +35,7 @@ using Xamarin.Forms.Xaml;
                 Ingredients = viewModel.Ingredients,
                 Preparation = viewModel.Preparation
             };
-            string ingredients = string.Join("\n", information.Ingredients.Select(i => i.Description));
+            string ingredients = string.Join("\n", information.Ingredients.Select(i => i.Amount), " ", information.Ingredients.Select(i => i.Name));
             string preparation = string.Join("\n\n", information.Preparation);
             Share.RequestAsync(new ShareTextRequest
             {
@@ -50,11 +49,25 @@ using Xamarin.Forms.Xaml;
 
             MessagingCenter.Unsubscribe<RecipePage, Recipe>(this, "ShareRecipe");
         }
-        private  void Shopping_Button(object sender, EventArgs e)
+        private async void AddToShoppingListButtonClicked(object sender, EventArgs e)
         {
-            PopupNavigation.Instance.PushAsync(new PopUpPage());
+            var currentUser = ((App)App.Current).currentUser;
 
+            if (currentUser == 0)
+            {
+                bool answer = await App.Current.MainPage.DisplayAlert("Add To Shopping List", "You have to sign in first to add this recipe to your shopping list. Do you want to sign in?", "Yes", "No");
+                if (answer)
+                {
+                    await App.Current.MainPage.Navigation.PushAsync(new SignInPage());
+                }
+            }
+            else
+            {
+                var modal = new ShoppingListModal(viewModel.Ingredients);
+                await Navigation.PushModalAsync(modal);
+            }
 
         }
+
     }
 }
